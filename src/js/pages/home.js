6f825abc5431c01891704450b193b165b1490e25
Dima <dimonmi1337@gmail.com>
2021-09-26 20:59:35 +0300

    test commit

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -3,7 +3,7 @@ import routes from '../routes.js'
 const viewDetailsBtn = document.getElementById('view-details-btn')
 const createMovieBtn = document.getElementById('create-movie-btn')
 const editMovieBtn = document.getElementById('edit-movie-btn')
-const testBtn = document.getElementById('test-btn')
+
 
 viewDetailsBtn.addEventListener('click', () => {
   window.location.href = routes.movieDetails({ name: 'lol', age: '32', status: 'accept' })
@@ -17,6 +17,4 @@ editMovieBtn.addEventListener('click', () => {
   window.location.href = routes.editMovie({ id: '123' })
 })
 
-testBtn.addEventListener('click', () => {
-  window.location.href = routes.testPage
-})
+
