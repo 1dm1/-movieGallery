fd1c8b2f2965d13c706a3cd17a41c27fccad5d5e
Dima <dimonmi1337@gmail.com>
2021-10-12 20:22:31 +0300

    refact

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,13 +1,26 @@
+
+
+
 const origin = window.location.origin
 const queryString = (params) => {
   const isObject = Boolean(typeof params === 'object' && Object.keys(params).length && !Array.isArray(params));
   return isObject ? `?${Object.keys(params).map(key => key + '=' + params[key]).join('&')}` : ''
 }
 
-export default {
+const routes = {
   home: `${origin}`,
   movieDetails: (params) => `${origin}/movie-details.html${queryString(params)}`,
   createMovie: `${origin}/create-movie.html`,
   editMovie: (params) => `${origin}/edit-movie.html${queryString(params)}`,
   testPage: `${origin}/test-page.html`,
 }
+
+export const redirect = (page,params) => {
+  if(params) {
+    for(let i in routes) {(i === page) ? window.location.href = routes[i](params) : ''}
+  }else {
+    window.location.href = routes[page]
+  }
+}
+
+
